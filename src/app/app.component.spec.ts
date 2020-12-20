e7c28615894c7f739e259291d2925222c5118e2f
Abhishek Kumar Pandey <abhishekpandey.kr@gmail.com>
2020-12-20 10:42:04 +0530

    test cases added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,42 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs/internal/observable/of';
 import { AppComponent } from './app.component';
+import { IFilter } from './interfaces/filter';
+import { ILaunces } from './interfaces/launches';
+import { SpacexLaunchService } from './services/spacex-launch.service';
 
 describe('AppComponent', () => {
+  const mockResponse: ILaunces[] = [
+    {
+      crew: 'alpha',
+      launch_date_utc: 12345,
+      links: {
+        mission_patch: 'dont know',
+        article_link: 'link',
+        mission_patch_small: null,
+        wikipedia: null,
+      },
+      mission_name: 'alpha test',
+      launch_year: '2009',
+      launch_success: true,
+      mission_id: [],
+    },
+  ];
+  let spacexMockService = jasmine.createSpyObj('SpacexLaunchService', [
+    'getAllLaunches',
+  ]);
+
+  spacexMockService.getAllLaunches.and.returnValue(of(mockResponse));
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: SpacexLaunchService, useValue: spacexMockService },
       ],
     }).compileComponents();
   }));
@@ -30,6 +57,20 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('space-x app is running!');
+    expect(compiled.querySelector('.title').textContent).toContain(
+      'SpaceX Launch Programs'
+    );
+  });
+
+  it('#applyFilter should call _getAllLaunches', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const filter: IFilter = {
+      launch_year: null,
+      launch_success: null,
+      land_success: null,
+    };
+    app.applyFilter(filter);
+    expect(spacexMockService.getAllLaunches).toHaveBeenCalled();
   });
 });
--- src/app/filters/filters.component.spec.ts
+++ src/app/filters/filters.component.spec.ts
@@ -8,9 +8,8 @@ describe('FiltersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FiltersComponent ]
-    })
-    .compileComponents();
+      declarations: [FiltersComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +20,37 @@ describe('FiltersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render Heading Filters', () => {
+    fixture = TestBed.createComponent(FiltersComponent);
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.filter_container').textContent).toContain(
+      'Filters'
+    );
+  });
+
+  it('should select the choosen year', () => {
+    spyOn(component.filterValuse, 'emit');
+    component.selectedYear({ year: 2010, selectable: false });
+    expect(component.filterValuse.emit).toHaveBeenCalled();
+  });
+
+  it('should select the choosen launch', () => {
+    spyOn(component.filterValuse, 'emit');
+    component.successfullLaunch({ value: 'False', selectable: false });
+    expect(component.filterValuse.emit).toHaveBeenCalled();
+  });
+
+  it('should select the choosen ', () => {
+    spyOn(component.filterValuse, 'emit');
+    component.successfullLanding({ value: 'False', selectable: false });
+    expect(component.filterValuse.emit).toHaveBeenCalled();
+  });
+
+  it('reset Filter should emit the default filter', () => {
+    spyOn(component.filterValuse, 'emit');
+    component.resetFilter();
+    expect(component.filterValuse.emit).toHaveBeenCalled();
   });
 });
